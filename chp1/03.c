80e939fa4aa15b2eb1365a48bee6f83181379c4c
Daniel Silva <daniel.silva@inf.ufrgs.br>
2013-07-17 15:15:15 -0300

    swapped 1.3 and 1.5

--- chp1/03.c
+++ chp1/03.c
@@ -2,18 +2,23 @@
 
 /* Fahrenheit to Celsius table */
 
-#define LOWER 0
-#define UPPER 300
-#define STEP  20
-
 int main ()
 {
-	float fahr;
+	float fahr, celsius;
+	int start, end, step;
+	
+	start = 0;
+	end   = 300;
+	step  = 20;
 	
 	printf (" F \t  C  \n");
 	printf ("---\t-----\n");
-	for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
-		printf ("%3.0f\t%5.1f\n", fahr, 5.0/9.0 * (fahr - 32.0));
+	fahr = start;
+	while (fahr <= end) {
+		celsius = 5.0/9.0 * (fahr - 32.0);
+		printf ("%3.0f\t%5.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
 	
 	return 0;
 }
--- chp1/05.c
+++ chp1/05.c
@@ -2,23 +2,18 @@
 
 /* Fahrenheit to Celsius table */
 
+#define LOWER 0
+#define UPPER 300
+#define STEP  20
+
 int main ()
 {
-	float fahr, celsius;
-	int start, end, step;
-	
-	start = 0;
-	end   = 300;
-	step  = 20;
+	float fahr;
 	
 	printf (" F \t  C  \n");
 	printf ("---\t-----\n");
-	fahr = start;
-	while (fahr <= end) {
-		celsius = 5.0/9.0 * (fahr - 32.0);
-		printf ("%3.0f\t%5.1f\n", fahr, celsius);
-		fahr = fahr + step;
-	}
+	for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
+		printf ("%3.0f\t%5.1f\n", fahr, 5.0/9.0 * (fahr - 32.0));
 	
 	return 0;
 }
